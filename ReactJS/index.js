b8f4d1a83e66d360d4fc2141d46bc4b6bcbd32e6
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-05-05 22:02:00 +0200

    Add files via upload

--- ReactJS/index.js
+++ ReactJS/index.js
@@ -1,4 +1,22 @@
-const h1 = document.createElement("h1")
-h1.textContent = "I love programming"
-h1.className="header"
-document.getElementById("root").append(h1)
+import React from "react"
+import ReactDOM from "react-dom"
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "I love programming"
+// h1.className="header"
+// document.getElementById("root").append(h1)
+
+//Creating a navbar in JSX
+
+const navbar = (
+    <nav>
+        <h1>KevinReactJs</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
